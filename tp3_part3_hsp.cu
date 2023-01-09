fc53c8ab78cbc53689ce9b30e2996bc43709c486
charlotte1esquirol <94962700+charlotte1esquirol@users.noreply.github.com>
2023-01-09 17:45:28 +0100

    Update tp3_part3_hsp.cu

--- tp3_part3_hsp.cu
+++ tp3_part3_hsp.cu
@@ -44,34 +44,29 @@ void MatrixPrint(float *M, int n, int p){
 
 
 //Addition de deux matrices sur GPU
-__device__ float* cudaMatrixAdd(float *M1, float *M2, float *Mout, int n, int m){
-    int i = threadIdx.x;
-    int j = threadIdx.y;
-
+__device__ void cudaMatrixAdd(float *M1, float *M2, float *Mout, int n, int m){
+    int i = threadIdx.x;//0
+    int j = threadIdx.y;//0->399
     
     if (i<n && j<m){
         Mout[i*m+j]=M1[i*m+j]+M2[i*m+j];
     }
-    return Mout;
 
 }
 
 
 
 //Multiplication de deux matrices sur GPU
-__device__ float* cudaMatrixMult3D(float *M1, float *M2, float *Mout, int n, int m, int p){
-    int i = threadIdx.x;
-    int j = threadIdx.y;
+__device__ void cudaMatrixMult(float *M1, float *M2, float *Mout, int n, int p, int q){
+    int i = threadIdx.x;//0
+    int j = threadIdx.y;//0-399
 
     float sum=0.0;
     
-    if (i<n && j<m){
-        for(int k=0; k<n; k++){
-            sum+=M1[i*p+k]*M2[k*m+j];
-        }
-            Mout[i*m+j]=sum;
+    for(int k=0; k<p; k++){
+        sum+=M1[i*p+k]*M2[k*q+j];
     }
-    return Mout;
+    Mout[i*q+j]=sum;
 
 }
 
@@ -159,14 +154,41 @@ __device__ float* activation_tanh(float* M, int M_nb_raw, int M_nb_column, int M
 
 }
 
+__device__ int activation_softmax(float* M, int M_nb_column){
+
+
+    float max;
+    int a;
+
+    max=0.0;
+    a=0;
+
+        for(int k=0;k<M_nb_column;k++){
+
+            if(M[k]>max){
+                a=k;
+            }
+
+        }
+
+    return a;
+
+}
+
 __global__ void cudaActivation_tanh(float* M,  int M_nb_raw, int M_nb_column, int M_nb_filtre){
     activation_tanh(M, M_nb_raw, M_nb_column, M_nb_filtre);
 }
 
+__global__ void cudaActivation_softmax(float* M, int M_nb_column){
+    activation_softmax(M,M_nb_column);
+}
 
-__global__ void cudaDense(float* M, float* Mout, float* W, float* B, int M_nb_raw, int M_nb_column, int M_nb_filtre){
-    Mout=cudaMatrixMult3D(M, W, Mout, M_nb_raw, M_nb_column, M_nb_filtre);
-    Mout=cudaMatrixAdd(Mout, B, Mout, M_nb_raw, M_nb_column);
+
+__global__ void cudaDense(float* M, float* Mout, float* W, float* B, int M_nb_raw, int M_nb_column, int W_nb_column){
+    float *Mout_bis;
+    Mout_bis = (float*)malloc(M_nb_raw*W_nb_column*1*sizeof(float));
+    cudaMatrixMult(M, W, Mout_bis, M_nb_raw, M_nb_column, W_nb_column);
+    cudaMatrixAdd(Mout_bis, B, Mout, M_nb_raw, W_nb_column);
 }
 
 
@@ -219,16 +241,14 @@ int main(){
     MatrixInit(F1_data,1,400,1,0);
     //MatrixPrint(F1_data,1,400);
 
-
-
     float *W1_kernel;
     W1_kernel = (float*)malloc(400*120*sizeof(float));
-    MatrixInit(W1_kernel,400,120,1,0);
+    MatrixInit(W1_kernel,400,120,1,1);
     //MatrixPrint(W1_kernel,400,120);
 
     float *B1_kernel;
     B1_kernel = (float*)malloc(120*sizeof(float));
-    MatrixInit(B1_kernel,1,120,1,0);
+    MatrixInit(B1_kernel,1,120,1,1);
     //MatrixPrint(B1_kernel,1,120);
 
     float *D1_data;
@@ -236,16 +256,14 @@ int main(){
     MatrixInit(D1_data,1,120,1,0);
     //MatrixPrint(D1_data,1,120);
 
-
-
     float *W2_kernel;
     W2_kernel = (float*)malloc(120*84*sizeof(float));
-    MatrixInit(W2_kernel,120,84,1,0);
+    MatrixInit(W2_kernel,120,84,1,1);
     //MatrixPrint(W2_kernel,120,84);
 
     float *B2_kernel;
     B2_kernel = (float*)malloc(84*sizeof(float));
-    MatrixInit(B2_kernel,1,84,1,0);
+    MatrixInit(B2_kernel,1,84,1,1);
     //MatrixPrint(B2_kernel,1,84);
 
     float *D2_data;
@@ -257,12 +275,12 @@ int main(){
 
     float *W3_kernel;
     W3_kernel = (float*)malloc(84*10*sizeof(float));
-    MatrixInit(W3_kernel,84,10,1,0);
+    MatrixInit(W3_kernel,84,10,1,1);
     //MatrixPrint(W3_kernel,84,10);
 
     float *B3_kernel;
     B3_kernel = (float*)malloc(10*sizeof(float));
-    MatrixInit(B3_kernel,1,10,1,0);
+    MatrixInit(B3_kernel,1,10,1,1);
     //MatrixPrint(B3_kernel,1,10);
 
     float *D3_data;
@@ -294,7 +312,6 @@ int main(){
      float *B3_kernel_g;
      float *D3_data_g;
 
-
      cudaMalloc((void**)&raw_data_g, 32*32*1*sizeof(float));
      cudaMalloc((void**)&C1_data_g, 28*28*6*sizeof(float));
      cudaMalloc((void**)&S1_data_g, 14*14*6*sizeof(float));
@@ -302,8 +319,6 @@ int main(){
      cudaMalloc((void**)&C2_kernel_g, 5*5*16*sizeof(float));
      cudaMalloc((void**)&C2_data_g, 10*10*16*sizeof(float));
      cudaMalloc((void**)&S2_data_g, 5*5*16*sizeof(float));
-     cudaMalloc((void**)&F1_data_g, 1*400*1*sizeof(float));
-     cudaMalloc((void**)&W1_kernel_g, 400*120*1*sizeof(float));
      cudaMalloc((void**)&B1_kernel_g, 1*120*1*sizeof(float));
      cudaMalloc((void**)&D1_data_g, 1*120*1*sizeof(float));
      cudaMalloc((void**)&W2_kernel_g, 120*84*1*sizeof(float));
@@ -313,9 +328,6 @@ int main(){
      cudaMalloc((void**)&B3_kernel_g, 1*10*1*sizeof(float));
      cudaMalloc((void**)&D3_data_g, 1*10*1*sizeof(float));
      
-
-    
-
      cudaMemcpy(raw_data_g, raw_data, 32*32*1*sizeof(float), cudaMemcpyHostToDevice);
      cudaMemcpy(C1_data_g, C1_data, 28*28*6*sizeof(float), cudaMemcpyHostToDevice);
      cudaMemcpy(S1_data_g, S1_data, 14*14*6*sizeof(float), cudaMemcpyHostToDevice);
@@ -334,65 +346,108 @@ int main(){
      cudaMemcpy(B3_kernel_g, B3_kernel, 1*10*1*sizeof(float), cudaMemcpyHostToDevice);
      cudaMemcpy(D3_data_g, D3_data, 1*10*1*sizeof(float), cudaMemcpyHostToDevice);
 
-     dim3 gridDim2(1,1);
-     dim3 blockDim2(28,28);
+     dim3 gridDim1(1,1);
+     dim3 blockDim1(28,28);
 
-     dim3 gridDim3(1,1);
-     dim3 blockDim3(10,10);
+    
      // Application de la première convolution 2D
-     cudaConv2D<<<gridDim2,blockDim2>>>(raw_data_g, C1_kernel_g, C1_data_g, 32, 32, 5, 6, 28, 28);
-     cudaDeviceSynchronize();
+     cudaConv2D<<<gridDim1,blockDim1>>>(raw_data_g, C1_kernel_g, C1_data_g, 32, 32, 5, 6, 28, 28);
+     cudaDeviceSynchronize();    
+
      // Application de la fonction d'activation Tanh
-     cudaActivation_tanh<<<gridDim2,blockDim2>>>(C1_data_g, 28, 28, 6);
+     cudaActivation_tanh<<<gridDim1,blockDim1>>>(C1_data_g, 28, 28, 6);
      cudaDeviceSynchronize();
      // Application du sous-échantillonnage
-     cudaSubsampling<<<gridDim2,blockDim2>>>(C1_data_g, S1_data_g, 28, 28, 6, 2, 14, 14);
+     cudaSubsampling<<<gridDim1,blockDim1>>>(C1_data_g, S1_data_g, 28, 28, 6, 2, 14, 14);
      cudaDeviceSynchronize();
      
-     
+      dim3 gridDim2(1,1);
+     dim3 blockDim2(10,10);
+
      // Application de la deuxième convolution
-     cudaConv2D<<<gridDim3,blockDim3>>>(S1_data_g, C2_kernel_g, C2_data_g, 14, 14, 5, 16, 10, 10);
+     cudaConv2D<<<gridDim2,blockDim2>>>(S1_data_g, C2_kernel_g, C2_data_g, 14, 14, 5, 16, 10, 10);
      cudaDeviceSynchronize();
      // Application de la fonction d'activation Tanh
-     cudaActivation_tanh<<<gridDim3,blockDim3>>>(C2_data_g, 10, 10, 16);
+     cudaActivation_tanh<<<gridDim2,blockDim2>>>(C2_data_g, 10, 10, 16);
      cudaDeviceSynchronize();
      // Application du sous-échantillonnage
-     cudaSubsampling<<<gridDim3,blockDim3>>>(C2_data_g, S2_data_g, 10, 10, 16, 2, 5, 5);
+     cudaSubsampling<<<gridDim2,blockDim2>>>(C2_data_g, S2_data_g, 10, 10, 16, 2, 5, 5);
      cudaDeviceSynchronize();
-     
+
      // Application de Flatten
      // Ici on sait que C2_data_g est comme flattend
 
+
+     
+
+
+     dim3 gridDim4(1,1);
+     dim3 blockDim4(1,120*400);
+
      // Application de Dense1
-     cudaDense<<<gridDim2,blockDim2>>>(S2_data_g, D1_data_g, W1_kernel_g, B1_kernel_g, 1, 400, 1);
+     cudaDense<<<gridDim4,blockDim4>>>(S2_data_g, D1_data_g, W1_kernel_g, B1_kernel_g, 1, 400, 120);
      cudaDeviceSynchronize();
+
+     
+     dim3 gridDim5(1,1);
+     dim3 blockDim5(1,120*84);
+
      //Application de la fonction d'activation Tanh
-     cudaActivation_tanh<<<gridDim2,blockDim2>>>(D1_data_g, 1, 120, 1);
+     cudaActivation_tanh<<<gridDim1,blockDim1>>>(D1_data_g, 1, 120, 1);
      cudaDeviceSynchronize();
+
+
+     
+     
+     
+
 
      // Application de Dense2
-     cudaDense<<<gridDim2,blockDim2>>>(D1_data_g, D2_data_g, W2_kernel_g, B2_kernel_g, 1, 120, 1);
-     cudaDeviceSynchronize();
-     //Application de la fonction d'activation Tanh
-     cudaActivation_tanh<<<gridDim2,blockDim2>>>(D2_data_g, 1, 84, 1);
+     cudaDense<<<gridDim5,blockDim5>>>(D1_data_g, D2_data_g, W2_kernel_g, B2_kernel_g, 1, 120, 84);
      cudaDeviceSynchronize();
 
-     // Application de Dense3
-     cudaDense<<<gridDim2,blockDim2>>>(D2_data_g, D3_data_g, W3_kernel_g, B3_kernel_g, 1, 84, 1);
-     cudaDeviceSynchronize();
+     dim3 gridDim6(1,1);
+     dim3 blockDim6(1,84*10);
+
      //Application de la fonction d'activation Tanh
-     cudaActivation_tanh<<<gridDim2,blockDim2>>>(D3_data_g, 1, 10, 1);
+     cudaActivation_tanh<<<gridDim6,blockDim6>>>(D2_data_g, 1, 84, 1);
+     cudaDeviceSynchronize();
+
+     cudaMemcpy(C1_data, C1_data_g, 28*28*6*sizeof(float), cudaMemcpyDeviceToHost);
+     cudaMemcpy(S1_data, S1_data_g, 14*14*6*sizeof(float), cudaMemcpyDeviceToHost);
+     cudaMemcpy(C2_data, C2_data_g, 10*10*16*sizeof(float), cudaMemcpyDeviceToHost);
+     cudaMemcpy(S2_data, S2_data_g, 5*5*16*sizeof(float), cudaMemcpyDeviceToHost);
+     cudaMemcpy(D1_data, D1_data_g, 1*120*1*sizeof(float), cudaMemcpyDeviceToHost);
+     cudaMemcpy(D2_data, D2_data_g, 1*84*1*sizeof(float), cudaMemcpyDeviceToHost);
+     printf("Matrice après convolution 2 2D et fonction d'activation :\n");
+     MatrixPrint(C2_data,10,10);
+     printf("Matrice après sous-échantillonnage:\n");
+     MatrixPrint(S2_data,5,5);
+     printf("Matrice après sous-échantillonnage:\n");
+     MatrixPrint(S2_data,5,5);
+     printf("Matrice flatten:\n");
+     MatrixPrint(S2_data,1,400);
+     printf("Matrice après Dense 1:\n");
+     MatrixPrint(D1_data,1,120);
+     printf("Matrice après Dense 2:\n");
+     MatrixPrint(D2_data,1,84);
+
+     // Application de Dense3
+     cudaDense<<<gridDim6,blockDim6>>>(D2_data_g, D3_data_g, W3_kernel_g, B3_kernel_g, 1, 84, 10);
+     cudaDeviceSynchronize();
+     //Application de la fonction d'activation Softmax
+     cudaActivation_softmax<<<gridDim1,blockDim1>>>(D3_data_g, 10);
      cudaDeviceSynchronize();
 
 
      cudaMemcpy(C1_data, C1_data_g, 28*28*6*sizeof(float), cudaMemcpyDeviceToHost);
      cudaMemcpy(S1_data, S1_data_g, 14*14*6*sizeof(float), cudaMemcpyDeviceToHost);
-     cudaMemcpy(C2_data, C2_data_g, 10*10*16*sizeof(float), cudaMemcpyHostToDevice);
-     cudaMemcpy(S2_data, S2_data_g, 5*5*16*sizeof(float), cudaMemcpyHostToDevice);
-     cudaMemcpy(F1_data, F1_data_g, 1*400*1*sizeof(float), cudaMemcpyHostToDevice);
-     cudaMemcpy(D1_data, D1_data_g, 1*120*1*sizeof(float), cudaMemcpyHostToDevice);
-     cudaMemcpy(D2_data, D2_data_g, 1*84*1*sizeof(float), cudaMemcpyHostToDevice);
-     cudaMemcpy(D3_data, D3_data_g, 1*10*1*sizeof(float), cudaMemcpyHostToDevice);
+     cudaMemcpy(C2_data, C2_data_g, 10*10*16*sizeof(float), cudaMemcpyDeviceToHost);
+     cudaMemcpy(S2_data, S2_data_g, 5*5*16*sizeof(float), cudaMemcpyDeviceToHost);
+     cudaMemcpy(F1_data, F1_data_g, 1*400*1*sizeof(float), cudaMemcpyDeviceToHost);
+     cudaMemcpy(D1_data, D1_data_g, 1*120*1*sizeof(float), cudaMemcpyDeviceToHost);
+     cudaMemcpy(D2_data, D2_data_g, 1*84*1*sizeof(float), cudaMemcpyDeviceToHost);
+     cudaMemcpy(D3_data, D3_data_g, 1*10*1*sizeof(float), cudaMemcpyDeviceToHost);
      cudaDeviceSynchronize();
 
 
@@ -416,39 +471,40 @@ int main(){
      printf("Matrice kernel W1:\n");
      //MatrixPrint(W1_kernel,400,120);
      printf("Matrice kernel B1:\n");
-     //MatrixPrint(B1_kernel,1,120);
+     MatrixPrint(B1_kernel,1,120);
      printf("Matrice après Dense 1:\n");
-     //MatrixPrint(D1_data,1,120);
+     MatrixPrint(D1_data,1,120);
      printf("Matrice kernel W2:\n");
-     //MatrixPrint(W2_kernel,120,84);
+     /*MatrixPrint(W2_kernel,120,84);
      printf("Matrice kernel B2:\n");
-     //MatrixPrint(B2_kernel,1,84);
+     MatrixPrint(B2_kernel,1,84);
      printf("Matrice après Dense 2:\n");
-     //MatrixPrint(D2_data,1,84);
+     MatrixPrint(D2_data,1,84);
      printf("Matrice kernel W3:\n");
-     //MatrixPrint(W3_kernel,84,10);
+     MatrixPrint(W3_kernel,84,10);
      printf("Matrice kernel B3:\n");
-     //MatrixPrint(B3_kernel,1,10);
+     MatrixPrint(B3_kernel,1,10);
      printf("Matrice après Dense 3:\n");
-     //MatrixPrint(D3_data,1,10);
+     MatrixPrint(D3_data,1,10);
+    */
      
 
      cudaFree(raw_data_g);
-     cudaFree(C1_kernel);
-     cudaFree(C1_data);
-     cudaFree(S1_data);
-     cudaFree(C2_data);
-     cudaFree(S2_data);
-     cudaFree(F1_data);
-     cudaFree(W1_kernel);
-     cudaFree(B1_kernel);
-     cudaFree(D1_data);
-     cudaFree(W2_kernel);
-     cudaFree(B2_kernel);
-     cudaFree(D2_data);
-     cudaFree(W3_kernel);
-     cudaFree(B3_kernel);
-     cudaFree(D3_data);
+     cudaFree(C1_kernel_g);
+     cudaFree(C1_data_g);
+     cudaFree(S1_data_g);
+     cudaFree(C2_data_g);
+     cudaFree(S2_data_g);
+     cudaFree(F1_data_g);
+     cudaFree(W1_kernel_g);
+     cudaFree(B1_kernel_g);
+     cudaFree(D1_data_g);
+     cudaFree(W2_kernel_g);
+     cudaFree(B2_kernel_g);
+     cudaFree(D2_data_g);
+     cudaFree(W3_kernel_g);
+     cudaFree(B3_kernel_g);
+     cudaFree(D3_data_g);
 
      free(raw_data);
      free(C1_kernel);
@@ -470,3 +526,4 @@ int main(){
 
     return 0;
     }
+
